149834bd9b3bfded7d03e796129927d7f637b2d9
ינוק <u9268782@bsmch.net>
2024-04-30 15:43:35 +0000

    creation

--- app.py
+++ app.py
@@ -3,9 +3,18 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
+from flask import Flask, render_template, session
 app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 @app.route("/")
 def hello():
-    return app.send_static_file("index.html")
+    session["count"] = 0
+    return render_template("index.html", count=session.get("count", 0))
+
+@app.route("/increment")
+def increment():
+    session["count"] = session.get("count", 0) + 1
+    return session["count"].__str__()
